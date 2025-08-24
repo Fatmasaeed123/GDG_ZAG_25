a9ccf724b0a8a4c0de303edce8a0e3ef1fc9282d
Fatmasaeed123 <fs792757@gmail.com>
2025-08-25 01:09:21 +0300

    new task

---
+++ Task14/main.js
@@ -0,0 +1,29 @@
+//task1 
+//تكليف 2
+document.write("<h1 style='color: blue;font-size: 80px;font-weight: bold;text-align: center;font-family: Arial'> Elzero</h1>")
+//تكليف 5 
+console.table(["Elzero","Ahmed","Sameh","Gamal","Aya"])
+
+
+//Task2 
+//تكليف 2
+console.log(elzero.innerHTML); 
+console.log(typeof elzero); 
+
+//تكليف 3
+console.log(
+  "`" + 
+  "I'm In\n" + 
+  "\\\\\n" + 
+  "Love \\\\ \"\"\" '''\n" + 
+  "++ With ++\n" + 
+  "\\\"\"\"\\\"\"\"\n" + 
+  "\"\"JavaScript\"\"" + 
+  "``"
+);
+
+//تكليف4
+let a = 21;
+let b = 20;
+
+console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`); // _21_2021_2021_2021_20_
